643024035f2d16892bbb051b2ee565fca4db0b26
minmik <kimcm3310@gmail.com>
2020-08-12 16:47:54 +0900

    Update benchmark tools and scripts

--- F8131/f8131_benchmark.sh
+++ F8131/f8131_benchmark.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
-# This script is used to change some configurations of GPU power management via sysfs interface. This code is hardware-specific and cannot be applied to mobile SoCs other than Sony Xperia X Performance (F8131). It changes the configurations of the kernel of Android 8.0.0 Oreo for Adreno 530 in Snapdragon 820.
+# This script is used to change some configurations of GPU power management via sysfs interface.
+# This code is hardware-specific and cannot be applied to mobile SoCs other than Sony Xperia X Performance (F8131).
+# It changes the configurations of the kernel of Android 8.0.0 Oreo for Adreno 530 in Snapdragon 820.
+# Plus runs some benchmark tests for possible GPU frequencies.
+# Based on Tensorflow Lite Benchmark Tool
+
+PROJECT_HOME="$(pwd)/.."
 SYSFS_DIR="/sys/class/kgsl/kgsl-3d0"
+GRAPH_NAME="mobilenet_v2_1.0_224.tflite"
+# TODO: get user input for the GRAPH_NAME
+HOST_EXE="$PROJECT_HOME/tools/benchmark_model"
+HOST_GRAPH="$PROJECT_HOME/models/$GRAPH_NAME"
 BENCHMARK_DIR="/data/local/tmp"
-BENCHMARK_EXE="benchmark_model"
-BENCHMARK_GRAPH="lite-model_aiy_vision_classifier_food_V1_1.tflite"
+BENCHMARK_EXE="$BENCHMARK_DIR/benchmark_model"
+BENCHMARK_GRAPH="$BENCHMARK_DIR/$GRAPH_NAME"
 BENCHMARK_OPT="--use_gpu=true --use_fp16=true --num_runs=100"
 BENCHMARK_POST="| grep avg"
-BENCHMARK="$BENCHMARK_DIR/$BENCHMARK_EXE --graph=$BENCHMARK_DIR/$BENCHMARK_GRAPH $BENCHMARK_OPT $BENCHMARK_POST"
+BENCHMARK="$BENCHMARK_EXE --graph=$BENCHMARK_GRAPH $BENCHMARK_OPT $BENCHMARK_POST"
+# Do not allow any error
+
+
 # needs a rooted phone
 adb root
+
+if [ ! $(adb shell "su -c '[ -e $BENCHMARK_EXE ] && echo 1'") ]; then
+    adb push $HOST_EXE $BENCHMARK_EXE
+fi
+
+if [ ! $(adb shell "su -c '[ -e $BENCHMARK_GRAPH ] && echo 1'") ]; then
+    adb push $HOST_GRAPH $BENCHMARK_GRAPH
+fi
 
 adb shell "su -c 'echo 1 > $SYSFS_DIR/force_clk_on'"
 adb shell "su -c 'echo 10000000 > $SYSFS_DIR/idle_timer'"
---
+++ models/README.md
@@ -0,0 +1,2 @@
+# Models folder
+This folder is for storing tflite models for the project. The models are included in the .gitignore list. So it will not appear at GitHub. Pretrained tflite models can be found here: https://www.tensorflow.org/lite/guide/hosted_models
